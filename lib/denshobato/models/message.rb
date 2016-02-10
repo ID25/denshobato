9142e88e244ad327a798d6bc0e9775182738895b
ID25 <xid25x@gmail.com>
2016-02-10 19:01:01 +0300

    Add inverse of for models, and alias (message.conversation instead message.denshobato_conversation)

--- lib/denshobato/models/message.rb
+++ lib/denshobato/models/message.rb
@@ -3,9 +3,12 @@ module Denshobato
     self.table_name = 'denshobato_messages'
 
     # Belongs to conversation
-    belongs_to :conversation, class_name: '::Denshobato::Conversation'
+    belongs_to :denshobato_conversation, class_name: '::Denshobato::Conversation', inverse_of: :denshobato_messages
 
     # Validations
     validates :body, :conversation_id, :sender_id, presence: true
+
+    # Alias
+    alias conversation denshobato_conversation
   end
 end
