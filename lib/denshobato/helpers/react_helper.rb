558ce9181f59161c962984b4e52687d0d70108c0
ID25 <xid25x@gmail.com>
2016-02-12 07:35:00 +0300

    Add messages json to react

--- lib/denshobato/helpers/react_helper.rb
+++ lib/denshobato/helpers/react_helper.rb
@@ -1,7 +1,7 @@
 module Denshobato
   module ReactHelper
-    def render_denshobato_messages
-      content_tag(:div, '', id: 'denshobato-message-panel')
+    def render_denshobato_messages(messages)
+      content_tag(:div, '', id: 'denshobato-message-panel', data: { json: messages.to_json })
     end
   end
 end
