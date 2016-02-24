f418112dcadf99523130df11fea7655070101276
ID25 <xid25x@gmail.com>
2016-02-25 01:01:45 +0300

    Add trashed column to conversation migration generator

--- lib/generators/denshobato/migrations/create_conversations.rb
+++ lib/generators/denshobato/migrations/create_conversations.rb
@@ -1,6 +1,7 @@
 class CreateDenshobatoConversations < ActiveRecord::Migration
   def change
     create_table :denshobato_conversations do |t|
+      t.boolean    :trashed,   default: false
       t.references :sender,    polymorphic: true, index: { name: 'conversation_polymorphic_sender' }
       t.references :recipient, polymorphic: true, index: { name: 'conversation_polymorphic_recipient' }
 
