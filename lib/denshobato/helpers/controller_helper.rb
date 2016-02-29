44903ea7b5f2e33ffbd0e45c61eb00c3e92ee6f3
ID25 <xid25x@gmail.com>
2016-03-01 01:26:53 +0300

    Add user argument to user_in_conversation? method

--- lib/denshobato/helpers/controller_helper.rb
+++ lib/denshobato/helpers/controller_helper.rb
@@ -2,10 +2,10 @@ module Denshobato
   module ControllerHelper
     include Denshobato::HelperUtils
 
-    def user_in_conversation?(room)
-      # redirect_to :root, notice: 'You can`t join this conversation unless user_in_conversation?(@conversation)'
+    def user_in_conversation?(user, room)
+      # redirect_to :root, notice: 'You can`t join this conversation unless user_in_conversation?(current_account, @conversation)'
 
-      hato_conversation.where(id: room.id, sender: current_account).present? || hato_conversation.where(id: room.id, recipient: current_account).present?
+      hato_conversation.where(id: room.id, sender: user).present? || hato_conversation.where(id: room.id, recipient: user).present?
     end
 
     def conversation_exists?(sender, recipient)
